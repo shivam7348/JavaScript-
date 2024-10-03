235ecbfc087b9ae37346ff194462e9f3624c1010
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-10-03 12:28:53 +0530

    scoping in code

--- control_flow/01_basic.js
+++ control_flow/01_basic.js
@@ -1,16 +1,44 @@
-const temperature = 41
+// const temperature = 41
 
-if(temperature === 40) {
-   console.log(`less than 50`);
+// if(temperature === 40) {
+//    console.log(`less than 50`);
    
-}
-else{ 
-    console.log("greater than 50");
+// }
+// else{ 
+//     console.log("greater than 50");
     
-}
+// }
 
-// console.log(`greater than 50`);
-
+// // console.log(`greater than 50`);
 
 
     
+// if(3!= 4) {
+//  console.log("execution");
+ 
+// }
+// else {
+//     console.log(`not executed`);
+    
+// }
+
+
+// const temperature = 41
+
+// if ( temperature === 51) {
+//     console.log(`less than 50`);
+    
+// }
+// else {
+//     console.log(`temperature is greater than 50`);
+    
+// }
+// console.log("execute");
+
+const score = 200
+ if (score > 100) {
+    const power = "fly"
+    console.log(`User Power: ${power}`);
+    
+ }
+     console.log(`User Power: ${power}`);
