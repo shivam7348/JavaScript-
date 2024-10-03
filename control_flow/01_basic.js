c31addb4e3e28e612cdde010af5c22e077517d28
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-10-03 12:01:31 +0530

    for type checking ===

---
+++ control_flow/01_basic.js
@@ -0,0 +1,16 @@
+const temperature = 41
+
+if(temperature === 40) {
+   console.log(`less than 50`);
+   
+}
+else{ 
+    console.log("greater than 50");
+    
+}
+
+// console.log(`greater than 50`);
+
+
+
+    
