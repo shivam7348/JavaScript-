a194b18f2ef4d6979322cd9026d67424a4925e55
Shivam Kumar <106791774+shivam7348@users.noreply.github.com>
2024-10-04 13:18:47 +0000

    iteration like for loop

---
+++ Iteration/01_basics.js
@@ -0,0 +1,22 @@
+
+// for (let i = 0; i <= 100; i++) {
+//     const element = i;
+//     if(element == 5) {
+//         console.log(`5 is best number`);
+        
+//     }
+//     console.log(element);
+    
+    
+// }
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`OutterLoop ${i}`);
+    
+    for( let j = 0; j<=10; j++) {
+        console.log(`Inner loop ${j} inner loop${i}`);
+        
+        
+
+    }
+}
