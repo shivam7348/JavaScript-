265f42ab965b463668e8571e71664416c507ef98
Shivam Kumar <106791774+shivam7348@users.noreply.github.com>
2024-10-06 18:05:28 +0000

    Explicit retutn adn im;icit retirn in curly varaces

--- Iteration/01_basics.js
+++ Iteration/01_basics.js
@@ -1,22 +1,7 @@
+ const myNums = [ 1, 2, 3, 4, 5, 6, 7 , 8 ,9, 10]
 
-// for (let i = 0; i <= 100; i++) {
-//     const element = i;
-//     if(element == 5) {
-//         console.log(`5 is best number`);
-        
-//     }
-//     console.log(element);
-    
-    
-// }
-
-for (let i = 0; i <= 10; i++) {
-    console.log(`OutterLoop ${i}`);
-    
-    for( let j = 0; j<=10; j++) {
-        console.log(`Inner loop ${j} inner loop${i}`);
-        
-        
-
-    }
-}
+  const newNums  = myNums.filter( ( num) => {
+    return num > 2
+  })
+  console.log(newNums );
+  
