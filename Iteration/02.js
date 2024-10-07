22d35b21ab5d6ad83e77f2c20b1f359d6c783a37
Shivam Kumar <106791774+shivam7348@users.noreply.github.com>
2024-10-07 04:51:39 +0000

    map function started

---
+++ Iteration/02.js
@@ -0,0 +1,11 @@
+//Mapp function start
+
+const myNums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
+ const newNumbers = myNums.map( ( num) =>{ 
+    return  num  % 2
+})
+
+console.log(newNumbers);
+
+// For each MEthod
