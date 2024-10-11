81dbfebe350d8836acc2da5527e9353d27b483b5
Shivam Kumar <106791774+shivam7348@users.noreply.github.com>
2024-10-11 06:15:28 +0000

    join() it typeOf the value is string

---
+++ 01.array.js
@@ -0,0 +1,27 @@
+
+// myArray.push(4)
+// myArray.pop()
+// myArray.unshift(0)
+// myArray.shift()
+// console.log(myArray.indexOf(9));
+
+// const newArray = myArray.join()
+
+// console.log(myArray);
+// console.log( typeof newArray);
+
+
+// slice and splice 
+const myArray = [0,1,2,3,5]
+
+console.log("A" , myArray);
+
+const myn1 = myArray.slice(1, 3,)
+  console.log(myn1);
+ console.log( "B" , myArray);
+ 
+const myn2 = myArray.splice(1,3)
+console.log("C", myArray);
+console.log(myn2);
+
+
