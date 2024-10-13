354c3f938b0ce793ffbcf8ce69e3d178dba77a24
Shivam Kumar <106791774+shivam7348@users.noreply.github.com>
2024-10-13 18:01:04 +0000

    key values entries and spread ...

--- object.js/02.object.js
+++ object.js/02.object.js
@@ -2,4 +2,61 @@
 
  const tinderApp = {}
 
+  tinderApp.id = "123abc"
+  tinderApp.name = "sammy"
+  tinderApp.loogedIn = false
+
+// console.log(tinderApp);
+
+const regularUser = {
+    email : "sammy@example.com",
+    fullName : {
+        usersfullname : {
+            firstName : "hitsh",
+            lastname : "miss"
+        }
+    }
+}
+
+// console.log(regularUser.fullName.usersfullname.firstName + " " + regularUser.fullName.usersfullname.lastname);
+
+const obj1 = {1 : "a", 2: "b"}
+const obj2 = {3 : "b", 4: "a"}
+const obj4  = {5 : "a", 6: "d"}
+
+// const obj3 = { obj1, obj2}
+//  const obj3 = Object.assign({}, obj1, obj2, obj4)
+const obj3 = {...obj1, ...obj2, ...obj4}
+console.log(obj3);
+
+
+const users = [
+    {
+        id : 1,
+        email: "user@example.com"
+    },
+    {
+        id : 1,
+        email: "user@example.com"
+    },
+    {
+        id : 1,
+        email: "user@example.com"
+    },
+    {
+        id : 1,
+        email: "user@example.com"
+    }
+]
+
+users[1].email
 console.log(tinderApp);
+
+console.log(Object.keys(tinderApp));
+
+console.log(Object.values(tinderApp));
+console.log(Object.entries(tinderApp));
+
+
+console.log(tinderApp.hasOwnProperty('isLoggedIn'));
+
