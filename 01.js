018ca1bd11959ad278d3260ed579c2323a386ecf
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-10-13 17:17:23 +0530

    flat take a varaible() and infinity spread operator

--- 01.js
+++ 01.js
@@ -1,6 +1,6 @@
-const marvelHeroes = ["thor", "ironman", "spiderman"]
+// const marvelHeroes = ["thor", "ironman", "spiderman"]
 
-const dc = ["superman", "flash", "batman"]
+// const dc = ["superman", "flash", "batman"]
 
 // marvelHeroes.push(dc)
 //  marvelHeroes.push(dc)
@@ -8,11 +8,26 @@ const dc = ["superman", "flash", "batman"]
 // console.log(marvelHeroes);
 
 // console.log(marvelHeroes[3][1]);
-const all_heroes = marvelHeroes.concat(dc)
+// const all_heroes = marvelHeroes.concat(dc)
 
-console.log(all_heroes);
+// console.log(all_heroes);
 
-const allnewHeroes = [...marvelHeroes, ...dc]
-console.log(allnewHeroes);
+// const allnewHeroes = [...marvelHeroes, ...dc]
+// console.log(allnewHeroes);
 
 
+// const another_array  = [1,2,3, [4,5,6], 7, [6, 7, 8[4,6]]]
+
+// const real_another_array = another_array.flat(Infinity)
+
+// console.log(real_another_array);
+//    console.log(Array.isArray("Hitesh")) 
+//    console.log(Array.from({name: "hitesh"}));    // INtersting 
+   
+   let score1= 100
+   let score2  = 200
+   let score3 = 300
+
+   console.log(Array.of(score1, score2, score3));
+   
+   
