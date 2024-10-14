cf578eb754c343b676f9d4871f6f59edc5ae27aa
shivam7348 <106791774+shivam7348@users.noreply.github.com>
2024-10-14 23:04:27 +0530

    singleton object

---
+++ 03_objects.js
@@ -0,0 +1,17 @@
+// singleton constructor se singletone se banta  object ek hi object multiple isrances
+
+//  object litersals
+
+const course = {
+    coursename: "jsHindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+
+}
+
+
+// console.log(course.courseInstructor)
+
+const { courseInstructor } = course
+
+console.log(courseInstructor);
