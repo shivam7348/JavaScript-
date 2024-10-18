0c46c6af427c3b6cf534b1477cfe4b87b0a123a5
Shivam Kumar <106791774+shivam7348@users.noreply.github.com>
2024-10-18 08:15:51 -0700

    maps are unique

--- HigherOrder/04.js
+++ HigherOrder/04.js
@@ -6,6 +6,27 @@ const myObject = {
 }
 
 for (const key in myObject) {
-   console.log(`${key} for shortcut  for ${myObject[key]}`);
+//    console.log(`${key} for shortcut  for ${myObject[key]}`);
    
 }
+
+const programing = ['js', 'cpp', 'java', 'swift']
+for (const key in programing) {
+
+    // console.log(`${key} for shortcut  for ${programing[key]}`);
+    
+        
+    }
+
+const map = new Map()
+map.set("IN", "India")
+map.set("US", "United state of America")
+map.set("JP", "Japan")
+map.set("fr", "france")
+map.set("jan", "Jaunpur")
+ 
+for (const key in map) {
+    console.log(key);
+    
+    }
+
