c9c38734285bc1eda982a85131344fe6de022c1d
Shivam Kumar <106791774+shivam7348@users.noreply.github.com>
2024-10-18 07:53:09 -0700

    for of loop map are itrabel

--- HigherOrder/01.js
+++ HigherOrder/01.js
@@ -7,7 +7,7 @@
 const arr = [1, 2, 3, 4, 5]
  for (const num of arr) {
 
-    console.log(num);
+    // console.log(num);
     
     
  }
@@ -15,8 +15,29 @@ const arr = [1, 2, 3, 4, 5]
  const greetings = "Hello world"
 
  for (const word of greetings) {
-    console.log(`Each char is ${word}`);
-    
+    // console.log(`Each char is ${word}`);
     
  }
+ // Maps
 
+ const myMap = new Map() 
+ myMap.set("IN", "India")
+ myMap.set("US", "United state of America")
+ myMap.set("JP", "Japan")
+ myMap.set("JP", "Japan")
+
+//  console.log(myMap);
+ 
+for (const [key, value] of myMap) {
+    // console.log(key, ':-', value);
+    
+}
+const myObject = {
+    'game1' : "cricket",
+    'game2' : "football",
+    'game3' : "hockey"
+}
+ for (const [key, value] of myObject) {
+     console.log(key, ":-", value);
+    
+ }
